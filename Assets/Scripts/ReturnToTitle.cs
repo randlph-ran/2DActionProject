007658c75b48663836a5dc318237881b2580c9ac
randlph-ran <randlph@gmail.com>
2026-06-04 18:55:00 +0900

    ゲームオーバー、ゲームクリアで自動でタイトル戻るように修正
    α版用の修正

---
+++ Assets/Scripts/ReturnToTitle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToTitle : MonoBehaviour
+{
+    // タイトルへ戻るまでの時間
+    [SerializeField]
+    private float returnTime = 5f;
+
+    private void Start()
+    {
+        // カウント開始
+        StartCoroutine(ReturnToTitleCoroutine());
+    }
+
+    /// <summary>
+    /// 一定時間後にタイトルへ戻る
+    /// </summary>
+    private IEnumerator ReturnToTitleCoroutine()
+    {
+        // 指定秒数待機
+        yield return new WaitForSeconds(returnTime);
+
+        // タイトルシーンへ移動
+        SceneManager.LoadScene("Title");
+    }
+}
